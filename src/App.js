c15178d89ae83df6e56eb534f12673f1c32bbce9
vshadrin <vshadrin@neoflex.ru>
2019-12-28 21:51:19 +0300

    initial

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Button from '@material-ui/core/Button';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [opt, setOpt] = useState(0);
+
+    const doOnclick = () => {
+        console.log(opt)
+        setOpt(opt === 0 ? 1 : 0)
+    }
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <Button color="primary" onClick={doOnclick}>
+                    test
+                </Button>
+            </header>
+            <div className="body-block">
+                {opt === 0 && (
+                    <div>BY!!!</div>
+                )
+                }
+                {opt === 1 && (
+                    <div>HELLO!!</div>
+                )
+                }
+            </div>
+        </div>
+    );
 }
 
 export default App;
